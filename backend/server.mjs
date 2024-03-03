c7005dfe84d6bcdae4baad8ad7f54619f85091da
Sam Gaines <samuelmgaines@gmail.com>
2024-03-03 00:59:06 +0000

    Metadata changes

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,21 +1,23 @@
 import express from "express";
 import './db.mjs'
+
 import studentController from './controllers/studentController.mjs'
 import majorController from './controllers/majorController.mjs'
 import categoryController from './controllers/categoryController.mjs'
 import locationController from './controllers/locationController.mjs'
-import cors from 'cors'
+
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-
-app.use(express.json())
-app.use('/api', studentController)
-app.use('/api', majorController)
-app.use('/api', categoryController)
-app.use('/api', locationController)
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api', studentController);
+app.use('/api', majorController);
+app.use('/api', categoryController);
+app.use('/api', locationController);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server!');
