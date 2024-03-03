cb62badc3563dc9b8081dc631f6460cbde866db2
lmillet3804 <135183116+lmillet3804@users.noreply.github.com>
2024-03-02 22:15:05 -0600

    Side panel is retractable

--- frontend/src/HRPortal/Sidebar.js
+++ frontend/src/HRPortal/Sidebar.js
@@ -1,18 +1,19 @@
 import "../styles/Sidebar.css"
 import FilterCategory from "./FilterCategory";
 
-function Sidebar() {
+function Sidebar({visible}) {
     const filterCategories = getFilterCategories();
     const filterDivs = [];
     for (const [key, value] of Object.entries(filterCategories)) {
         filterDivs.push(<FilterCategory name={key} data={value}/>)
     }
     return(
-        <div class="sidebar">
+        <div class="sidebar" style={visible ? {display:"block"} : {display: "none"}}>
             <h2>Filters</h2>
             {filterDivs}
         </div>
-    );
+        
+    )
 } export default Sidebar;
 
 
